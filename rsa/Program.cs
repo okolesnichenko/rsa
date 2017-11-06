f55b9e02952498036995d3337c0bb3e3bf5b2c68
worldintro <worldintro@mail.ru>
2017-11-06 14:04:37 +0300

    Initial commit

---
+++ rsa/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsa
+{
+    class Program
+    { 
+        public struct Key
+        {
+            public uint keynum;
+            public ulong n;
+        }
+        static void Main(string[] args)
+        {
+            //2 больших и простых числа p и q, и d 
+            uint p = 7;
+            uint q = 4;
+            Random rnd = new Random();
+            uint d = Rsa.Calculate_d(p, q);
+            Console.WriteLine("d : " + d);
+            ulong n = p * q;
+
+            //шифруемый текст 
+            Console.Write("Type text: ");
+            string startText = Console.ReadLine();
+            string[] M = new string[n - 1];
+            string[] C = new string[n - 1];
+            string[] M1 = new string[n - 1];
+
+            //генерация ключей
+            Key openkey = Rsa.GenerateOpenKey(n, d, p, q);
+            Key secretkey = Rsa.GenerateSecretKey(n, d);
+
+            ulong lengthOfBlock = (ulong)(startText.Length) / n + 1;
+
+            //разделение текста на блоки
+                if(startText.Length<M.Length)
+                {
+                    for (int i = 0; i < startText.Length - 1; i++)
+                    {
+                        M[i] = startText.Substring(i, (int)lengthOfBlock);
+                    }
+                Array.Resize<string>(ref M, startText.Length);
+                }
+                else
+                {
+                    //пока не рабоатет
+                    for (int i = 0; i < M.Length - 2; i++)
+                    {
+                        M[i] = startText.Substring(i, (int)lengthOfBlock);
+                    }
+                }
+
+
+            //шифрование
+            Console.Write("Encode: ");
+            C = Rsa.Encode(M, openkey);
+            for(int i = 0; i<C.Length-1; i++)
+            {
+                if (C[i] != null)
+                {
+                    Console.Write(C[i]+" ");
+                }
+            }
+
+            //Дешифровка
+            Console.Write("Decode: ");
+            M1 = Rsa.Decode(C, secretkey);
+            for (int i = 0; i < M1.Length - 1; i++)
+            {
+                if (M1[i] != null)
+                {
+                    Console.Write(M1[i]+ " ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ rsa/Rsa.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace rsa
+{
+    class Rsa
+    {
+        static public Program.Key GenerateOpenKey(ulong n, uint d, uint p, uint q)
+        {
+            Program.Key key;
+            key.n = n;
+            key.keynum = Calculate_e(d, p, q);
+            return key;
+        }
+
+        static public Program.Key GenerateSecretKey(ulong n, uint d)
+        {
+            Program.Key key;
+            key.n = n;
+            key.keynum = d;
+            return key;
+        }
+        static public string[] Encode(string[] M, Program.Key openkey)
+        {
+            //C(i)=(M(I)^e)mod n, где bi=M(i)^e
+            Console.Write("Open: " + openkey.keynum + "|");
+            string[] C = new string[openkey.n-1];
+            BigInteger bi;
+            BigInteger n_ = (BigInteger)openkey.n;
+            for(int i = 0; i < M.Length; i++)
+            {
+                bi = (BigInteger)Math.Pow(i, openkey.keynum);
+                C[i] = (bi % n_).ToString();
+            }
+            return C ;
+        }
+
+        static public string[] Decode(string[] C, Program.Key secretkey)
+        {
+            Console.Write("Secret: " + secretkey.keynum +"|");
+            string[] M = new string[secretkey.n - 1];
+            BigInteger bi;
+            BigInteger n_ = (BigInteger)secretkey.n;
+            for (int i = 0; i < M.Length; i++)
+            {
+                bi = (BigInteger)Math.Pow((Convert.ToInt32(C[i])), secretkey.keynum);
+                M[i] = (bi % n_).ToString();
+            }
+            return M;
+        }
+        static uint Calculate_e(uint d, uint p, uint q)
+        {
+            uint e = 0;
+            while (true)
+            {
+                if ((e * d) % ((d-1)*(q-1)) == 1)
+                    break;
+                else
+                    e++;
+            }
+            return e;
+        }
+        static public uint Calculate_d(uint p, uint q)
+        {
+            uint d = (p-1)*(q-1) - 1;
+
+            for (uint i = 2; i <= (p - 1) * (q - 1); i++)
+                if (((p - 1) * (q - 1) % i == 0) && (d % i == 0)) //если имеют общие делители
+                {
+                    d--;
+                    i = 1;
+                }
+
+            return d;
+        }
+    }
+}
